bcce1015dfd904047837367f85a97d4c488b5068
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-07-27 09:38:25 +0100

    refactor to add image

--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -28,7 +28,9 @@ const page = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:8082/movies");
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
@@ -39,7 +41,7 @@ const page = () => {
       }
     };
 
-    fetchData();
+    // fetchData();
   }, []);
 
   // Custom hook called with initial state values
@@ -64,14 +66,19 @@ const page = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-      <div className="max-w-screen-xl:m-10 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+    <>
+      <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-          <div className="flex justify-center">
-            <Image src={sky} alt="Sky Logo" width={200} />
+          <div>
+            <Image
+              src={sky}
+              alt="Sky Logo"
+              width={200}
+              className="w-32 mx-auto"
+            />
           </div>
           <div className="mt-12 flex flex-col items-center">
-            <h1 className="text-2xl xl:text-3xl font-extrabold">
+            <h1 className="text-2xl xl:text-3xl font-extrabold text-black">
               Sign up for Sky Travel!
             </h1>
             <div className="w-full flex-1 mt-8">
@@ -118,93 +125,100 @@ const page = () => {
                   Or sign up with e-mail
                 </div>
               </div>
-              <form onSubmit={handleSubmit}>
-                <div className="mx-auto max-w-xs">
-                  <input
-                    type="text"
-                    name="name"
-                    value={name}
-                    onFocus={handleWarningOnFocus}
-                    onBlur={handleWarningOnBlur}
-                    onChange={handleInputChange}
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                    placeholder="name"
-                  />
 
-                  {warnings.name && errors.name?.length > 0 && (
-                    <small className="text-sm text-red-500">
-                      {errors.name[0]}
-                    </small>
-                  )}
-                  <input
-                    type="email"
-                    name="email"
-                    value={email}
-                    onFocus={handleWarningOnFocus}
-                    onBlur={handleWarningOnBlur}
-                    onChange={handleInputChange}
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                    placeholder="Email"
-                  />
-                  {warnings.email && errors.email?.length > 0 && (
-                    <small className="text-sm text-red-500">
-                      {errors.email[0]}
-                    </small>
-                  )}
-                  <input
-                    type="password"
-                    name="password"
-                    value={password}
-                    onFocus={handleWarningOnFocus}
-                    onBlur={handleWarningOnBlur}
-                    onChange={handleInputChange}
-                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                    placeholder="Password"
-                  />
-                  {warnings.password && errors.password?.length > 0 && (
-                    <small className="text-sm text-red-500">
-                      {errors.password[0]}
-                    </small>
-                  )}
-                  <button className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                    <svg
-                      className="w-6 h-6 -ml-2"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                      <circle cx="8.5" cy="7" r="4" />
-                      <path d="M20 8v6M23 11h-6" />
-                    </svg>
-                    <span className="ml-3">Sign Up</span>
-                  </button>
+              <div className="mx-auto max-w-xs">
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="text"
+                  placeholder="name"
+                  name="name"
+                  value={name}
+                  onFocus={handleWarningOnFocus}
+                  onBlur={handleWarningOnBlur}
+                  onChange={handleInputChange}
+                />
+                {warnings.name && errors.name?.length > 0 && (
+                  <small className="text-sm text-red-500">
+                    {errors.name[0]}
+                  </small>
+                )}
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={email}
+                  onFocus={handleWarningOnFocus}
+                  onBlur={handleWarningOnBlur}
+                  onChange={handleInputChange}
+                />
+                {warnings.email && errors.email?.length > 0 && (
+                  <small className="text-sm text-red-500">
+                    {errors.email[0]}
+                  </small>
+                )}
+                <input
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onFocus={handleWarningOnFocus}
+                  onBlur={handleWarningOnBlur}
+                  onChange={handleInputChange}
+                />
+                {warnings.password && errors.password?.length > 0 && (
+                  <small className="text-sm text-red-500">
+                    {errors.password[0]}
+                  </small>
+                )}
 
-                  <p className="mt-6 text-xs text-gray-600 text-center">
-                    I agree to Sky's&nbsp;
-                    <a
-                      href="#"
-                      className="border-b border-gray-500 border-dotted"
-                    >
-                      Terms of Service
-                    </a>
-                    &nbsp;and its&nbsp;
-                    <a
-                      href="#"
-                      className="border-b border-gray-500 border-dotted"
-                    >
-                      Privacy Policy
-                    </a>
-                  </p>
-                </div>
-              </form>
+                <button className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                  <svg
+                    className="w-6 h-6 -ml-2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy="7" r="4" />
+                    <path d="M20 8v6M23 11h-6" />
+                  </svg>
+                  <span className="ml-3">Sign Up</span>
+                </button>
+                <p className="mt-6 text-xs text-gray-600 text-center">
+                  I agree to abide by Sky's&nbsp;
+                  <a
+                    href="#"
+                    className="border-b border-gray-500 border-dotted"
+                  >
+                    Terms of Service&nbsp;
+                  </a>
+                  and its&nbsp;
+                  <a
+                    href="#"
+                    className="border-b border-gray-500 border-dotted"
+                  >
+                    Privacy Policy
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+          <div
+            className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+            style={{
+              backgroundImage:
+                "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')",
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 

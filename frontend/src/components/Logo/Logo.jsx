77b21ac044ff64929386ca587fa404468e81fa8c
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-07-27 20:14:31 +0100

    Create Logo + Default export
    By using an index.js file inside a folder, we can import all the components from one place.

---
+++ frontend/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+// components/Logo.js
+import Image from "next/image";
+import sky from "../../../public/assets/sky.png";
+
+const Logo = () => {
+  return (
+    <div>
+      <Image src={sky} alt="Sky Logo" width={200} className="w-32 mx-auto" />
+    </div>
+  );
+};
+
+export default Logo;

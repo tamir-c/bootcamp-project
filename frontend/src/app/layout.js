863d1452831107479f0ce32511cf7a9bbb37fcd0
Faseeh Lodhi <faseehlodhi980@gmail.com>
2023-07-28 12:56:35 +0100

    Header Navbar

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "../components/Navbar/Header.jsx"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
       {/* We will built up our components here */}
       <body className={inter.className}>
         {/* <h1>Hello, world</h1> */}
+        <Header />
         <main>{children}</main>
       </body>
     </html>

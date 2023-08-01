0bda6bfbabf735ebe3e799b08cd616a0f0cacbd2
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-08-01 08:24:54 +0100

    remove unused state

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,10 @@
 "use client";
 
 import MovieCard from "@/components/Cards/MovieCard";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { useThemeContext } from "@/components";
 
 export default function Home() {
+  const { movies } = useThemeContext();
   /**
    To get the data in any component, we can use the useThemeContext hook.
 
@@ -15,26 +14,13 @@ export default function Home() {
     return data.find((item) => item.id === id);
   };
    */
-  const { data } = useThemeContext();
-  // const BACKEND_URL = "http://127.0.0.1:8082";
-  const BACKEND_URL = "http://18.170.108.208:8082";
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const response = await axios.get(`${BACKEND_URL}/movies`);
-    const data = await response.data;
-    setMovies(data);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
 
   return (
     <div className="m-8 grid 2xl:grid-cols-8 xl:grid-cols-7 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-10 place-items-center">
       {movies?.length > 0 ? (
         movies.map((m) => (
           <div
+            key={m.movie_id}
             className="cursor-pointer"
             onClick={(id = m.movie_id) => {
               console.log(id);
